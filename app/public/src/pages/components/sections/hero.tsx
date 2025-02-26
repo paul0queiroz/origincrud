9695c9e4cf8d04bd6d1b9be6f9ab4531352086c0
paul0queiroz <queirozp0807@gmail.com>
2025-02-25 21:26:40 -0300

    feat: enhance Hero component with form submission and add custom text styles

--- app/public/src/pages/components/sections/hero.tsx
+++ app/public/src/pages/components/sections/hero.tsx
@@ -23,8 +23,19 @@ export default function Hero() {
       headers: {
        "Content-Type": "application/json",
       },
-      body: JSON,stringify(formData: any),
-    })
-  }
-  })
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    alert(data.message);
+  };
+  return(
+    <section className="bg-gradient-to-r from-indigo-400 to-blue-400 text-gray/50 text-title py-20 px-6 text-center">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-4xl text-title font-bold mb-4 bg-[linear-gradient(90deg, #f2f2f2,#e8e1ea,#aac2ee)] ">Cadastro de Contatos</h1>
+        <p className="">Gerenciar seus contatos nunca foi tão rápido e prático!</p>
+      </div>
+      
+      <form onSubmit={handleSubmit}></form>
+    </section>
+  )
 }
